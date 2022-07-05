a7dfc46c4c4876d76ce0956d77fccffef76b491e
Kate Fang <fm980513@126.com>
2022-07-04 19:08:44 -0600

    User registration and login module finished.

--- src/main/resources/static/js/global.js
+++ src/main/resources/static/js/global.js
@@ -1,3 +1,5 @@
+var CONTEXT_PATH = "/Community";
+
 window.alert = function(message) {
 	if(!$(".alert-box").length) {
 		$("body").append(
--- src/test/java/com/nowcoder/community/MapperTests.java
+++ src/test/java/com/nowcoder/community/MapperTests.java
@@ -1,12 +1,15 @@
 package com.nowcoder.community;
 
 import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.LoginTicketMapper;
 import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.LoginTicket;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
+import java.util.Date;
 import java.util.List;
 
 @SpringBootTest
@@ -15,6 +18,9 @@ public class MapperTests {
 
     @Autowired
     private DiscussPostMapper discussPostMapper;
+
+    @Autowired
+    private LoginTicketMapper loginTicketMapper;
 
     @Test
     public void testSelectPost(){
@@ -26,4 +32,25 @@ public class MapperTests {
         int rows = discussPostMapper.selectDiscussPostRows(149);
         System.out.println(rows);
     }
+
+//    @Test
+//    public void testInsertLoginTicket() {
+//        LoginTicket loginTicket = new LoginTicket();
+//        loginTicket.setUserId(101);
+//        loginTicket.setTicket("abc");
+//        loginTicket.setStatus(0);
+//        loginTicket.setExpired(new Date(System.currentTimeMillis() + 1000 * 60 * 10));
+//
+//        loginTicketMapper.insertLoginTicket(loginTicket);
+//    }
+//
+//    @Test
+//    public void testSelectLoginTicket() {
+//        LoginTicket loginTicket = loginTicketMapper.selectByTicket("abc");
+//        System.out.println(loginTicket);
+//
+//        loginTicketMapper.updateStatus("abc", 1);
+//        loginTicket = loginTicketMapper.selectByTicket("abc");
+//        System.out.println(loginTicket);
+//    }
 }
